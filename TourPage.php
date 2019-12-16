437e3e4836dbc008cf2bfd7cecb2fb9154075c13
developerjulie93 <56218979+developerjulie93@users.noreply.github.com>
2019-12-15 18:04:35 -0800

    Add files via upload

---
+++ TourPage.php
@@ -0,0 +1,223 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!--Reference
+		https://dribbble.com/shots/5685400-Copenhagen-Tours-Page?utm_source=Clipboard_Shot&utm_campaign=outcrowd&utm_content=Copenhagen%20Tours%20Page&utm_medium=Social_Share-->
+
+	<title>TourPage</title>
+	<link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:500" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+	<div class="section1">
+	<header>
+		<nav>
+			<div class="left">
+				<ul>
+					<li><a href="#">
+						<div id="logo"></div>
+					</a></li>
+					<li><a href="http://localhost/html/TourPage.php#container">Boat Tour</a></li>
+					<li><a href="http://localhost/html/TourPage.php#container">Bike Tour</a></li>
+					<li><a href="http://localhost/html/TourPage.php#container">Cable Car Tour</a></li>
+					<li><a href="http://localhost/html/TourPage.php#container">Panoramic Tour</a></li>
+				</ul>
+			</div>
+			<div class="right">
+				<ul>
+					<li><a href="http://localhost/html/BookingPage.html" class="right_btns" id="right1">Book</a></li>
+					<li><a href="http://localhost/html/TourPage.php#wrapper4" class="right_btns" id="right2">Contact Us</a></li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+	<div class="wrapper">
+		<h1 id="f1">CITY OF</h1>
+		<h1 id="f2">VANCOUVER</h1>
+
+		<div class="search_box">
+			<div class="box">
+				<div id="box1">Choose Location</div>
+				<div id="box2">Date</div>
+				<div id="box3">Guests</div>
+			</div>
+			<div class="search">
+				search
+			</div>
+		</div>
+	</div>
+	</div>
+	<div class="section2">
+		<div class="wrapper2">
+		<div class="container" id="container">
+			<i class="fas fa-map-marker-alt iconMap"></i>
+			<h1 style="font-size:32px; color: rgb(28,30,60);">
+      			Available Summer Tours</h1>
+		</div>
+		<div class="tickets">
+			<div class="ticket">
+				<div class="icons" id="boat">
+					<i class="fas fa-anchor"></i>
+				</div>
+				<div class="discription">
+					<h3>Hop On - Hop Off Boat (1 hour)</h3>
+					<p style="font-size: 14px; color: rgb(150,148,147);">Get onboard one of our Hop On - Hot Off boat tours.</p>
+				</div>
+				<div class="price">
+					<h2 style="color: rgb(255,206,0);">$25.00</h2>
+				</div>
+			</div>
+			<div class="ticket">
+				<div class="icons" id="bike">
+					<i class="fas fa-bicycle"></i>
+				</div>
+				<div class="discription">
+					<h3>Round City Bike Tour (1 hours)</h3>
+					<p style="font-size: 14px; color: rgb(150,148,147);">Get onboard one of our Round City Bike Tour.</p>
+				</div>
+				<div class="price">
+					<h2 style="color: rgb(255,206,0);">$15.00</h2>
+				</div>
+			</div>
+			<div class="ticket">
+				<div class="icons" id="cable">
+					<i class='fas fa-tram'></i>
+				</div>
+				<div class="discription">
+					<h3>City Cable Car Tour (1 hours)</h3>
+					<p style="font-size: 14px; color: rgb(150,148,147);">Get onboard one of our City Cable Car Tour.</p>
+				</div>
+				<div class="price">
+					<h2 style="color: rgb(255,206,0);">$25.00</h2>
+				</div>
+			</div>
+			<div class="ticket">
+				<div class="icons" id="car">
+					<i class="fas fa-car-side"></i>
+				</div>
+				<div class="discription">
+					<h3>Round City Panoramic (3 hours)</h3>
+					<p style="font-size: 14px; color: rgb(150,148,147);">Get onboard one of our Round City Panoramic Tour</p>
+				</div>
+				<div class="price">
+					<h2 style="color: rgb(255,206,0);">$85.00</h2>
+				</div>
+			</div>
+		</div>
+		</div>
+	</div>
+	<div class="section3">
+		<div class="wrapper3">
+			<div style="padding-bottom: 5px;">
+				<h1 style="color: rgb(255,255,255); font-size: 32px;">Discover Vancouver at</h1>
+				<h1 style="color: rgb(255,255,255);font-size: 32px;">your own pace</h1>
+			</div>
+			<div style="padding-bottom: 30px;">
+				<p style="color: rgb(255,255,255);">An easy and calm way to get to the other side of the harbor, and the ticket</p>
+				<p style="color: rgb(255,255,255);">you can enjoy a free beer or soda at the Reffen bars.</p>
+			</div>
+			<div>
+				<div class="btns" id="btn_book">Book a Tour</div>
+				<div class="btns" id="btn_info">More Info</div>
+			</div>
+		</div>
+	</div>
+	<div class="section4">
+		<div class="wrapper4">
+		<div class="container">
+			<i class="far fa-compass iconCompass"></i>
+			<h1 style="font-size:32px; color: rgb(28,30,60);">
+      			Granvile Island Boat Services</h1>
+		</div>
+		<div class="boat_info">
+			<div class="boat_ticket_boat">
+				<div>
+					<h2 style="color: rgb(0,88,151);">1. Tickets</h2>
+					<p>Granvile Island is one of  </p>
+					<p>Vancouver's up and coming area, </p>
+					<p>which is slowly turning</p>
+					<p>from an industrial wasteland into.</p>
+				</div>
+				<div>
+					<h2 style="color: rgb(251,204,5);">2. Boat</h2>
+					<p>The trendy street food market offers</p>
+					<p>fantastic treats for the taste buds.</p>
+					<p>You will discover a large array of</p>
+					<p>international cuisines and local.</p>
+				</div>
+			</div>
+			<div class="boat_explore">
+				<dir>
+					<h2 style="color: rgb(25,160,175);">3. Explore</h2>
+					<p>Granvile Island is one of </p>
+					<p>Vancouver's up and coming </p>
+					<p>areas, which is slowly turning</p>
+					<p>from an industrial wasteland into.</p>
+				</dir>
+			</div>
+		</div>
+		</div>
+	</div>
+	<div class="section5">
+		<div class="wrapper4" id="wrapper4">
+			<div>
+				<h1 style="font-size:32px; color: rgb(28,30,60);">Discover Vancouver</h1>
+				<h1 style="font-size:32px; color: rgb(28,30,60);">Places With Us</h1>
+
+				<p style="text-align: center; padding-top: 20px; color: rgb(127,124,144);">An easy and calm way to get to the other side of the harbor, and the</p>
+				<p style="text-align: center; color: rgb(127,124,144);">ticket you can enjoy a free beer or soda at the Granvile Island bars.</p>
+			</div>
+			<div class="input_container">
+				<div class="input">
+					<p>Name</p>
+				</div>
+				<div class="input">
+					<p>Phone</p>
+				</div>
+				<div class="input">
+					<p>E-mail</p>
+				</div>
+			</div>
+			<div id="send">
+				Send Request
+			</div>
+		</div>
+	</div>
+	<div class="section6">
+		<footer>
+			<nav>
+			<div class="left">
+				<ul>
+					<li><a href="#">
+						<div id="logo"></div>
+					</a></li>
+					<li><a href="http://localhost/html/TourPage.php#container">Boat Tour</a></li>
+					<li><a href="http://localhost/html/TourPage.php#container">Bike Tour</a></li>
+					<li><a href="http://localhost/html/TourPage.php#container">Cable Car Tour</a></li>
+					<li><a href="http://localhost/html/TourPage.php#container">Panoramic Tour</a></li>
+				</ul>
+			</div>
+			<div class="right">
+				<ul>
+					<li><a href="#">
+						<i class="fab fa-facebook-f"></i>
+					</a></li>
+					<li><a href="#">
+						<i class="fab fa-twitter"></i>
+					</a></li>
+					<li><a href="#">
+						<i class="fab fa-instagram"></i>
+					</a></li>
+				</ul>
+			</div>
+			</nav>
+		</footer>
+	</div>
+</body>
+</html>
